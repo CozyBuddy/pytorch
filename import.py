2b04e2fe94477c7a04e6b7236f6d37d2c49d22e9
malgeum <kwonsunny@naver.com>
2024-10-12 14:54:35 +0900

    66

---
+++ import.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+dataset = pd.read_csv('car_evaluation.csv')
+dataset.head()
+plt.show()
+
