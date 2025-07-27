ad43dee1ae528a2acd942c2b06f4727c12493c21
sunny <kwonsunny@naver.com>
2025-07-27 15:54:52 +0900

    노트북수정

--- SEOUL_FINEDUST2.py
+++ SEOUL_FINEDUST2.py
@@ -115,7 +115,7 @@ y_test_tensors = torch.tensor(y_test, dtype=torch.float32)
 class LSTM(nn.Module):
     def __init__(self, num_classes, input_size , hidden_size , num_layers , seq_length):
         super(LSTM , self).__init__()
-        self.num_classes = num_classes
+        # self.num_classes = num_classes
         self.num_layers = num_layers
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -123,8 +123,8 @@ class LSTM(nn.Module):
 
         self.lstm = nn.LSTM(input_size=input_size , hidden_size=hidden_size , num_layers=num_layers , batch_first=True)
 
-        self.fc_1 = nn.Linear(hidden_size ,128)
-        self.fc = nn.Linear(128 , num_classes)
+        self.fc_1 = nn.Linear(hidden_size ,512)
+        self.fc = nn.Linear(512 , num_classes)
         self.relu = nn.ReLU()
 
 
---
+++ 자연어처리심층학습.py
@@ -0,0 +1,6 @@
+import torch
+
+print(torch.tensor([1,2,3]))
+print(torch.Tensor([[1,2,3],[4,5,6]]))
+print(torch.LongTensor([1,2,3]))
+print(torch.FloatTensor([1,2,3]))
